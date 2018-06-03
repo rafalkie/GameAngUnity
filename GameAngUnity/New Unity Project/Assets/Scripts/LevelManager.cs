a0dd261866995bc66394a6ac0b28b211ec5e6a33
lckvv <lckorone@gmail.com>
2018-06-03 02:10:05 +0200

    otoczenie 1lvl
    spadnia, respawn, punkty

--- GameAngUnity/New Unity Project/Assets/Scripts/LevelManager.cs
+++ GameAngUnity/New Unity Project/Assets/Scripts/LevelManager.cs
@@ -7,9 +7,14 @@ public class LevelManager : MonoBehaviour {
 	public GameObject currentCheckpoint;
 	public int pointPenaltyOnDeath;
 
+	private playerControler player;
+
+	public GameObject deathParticle;
+	public GameObject respawnParticle;
 	public float respawnDelay;
 
-	private playerControler player;
+	private float gravityStore;
+
 	// Use this for initialization
 	void Start () {
 		player = FindObjectOfType<playerControler> ();
@@ -25,16 +30,19 @@ public class LevelManager : MonoBehaviour {
 	}
 
 	public IEnumerator RespawnPlayerCo(){
-		player.enabled = false;
+		Instantiate (deathParticle, player.transform.position, player.transform.rotation);
+		player.enabled = false;	
 		player.GetComponent<Renderer> ().enabled = false;
+		gravityStore = player.GetComponent<Rigidbody2D> ().gravityScale;
 		player.GetComponent<Rigidbody2D> ().gravityScale = 0f;
 		player.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
-
 		scoreManager.AddPoints (-pointPenaltyOnDeath);
 		Debug.Log ("Player Respawn");
 		yield return new WaitForSeconds (respawnDelay);
+		player.GetComponent<Rigidbody2D> ().gravityScale = gravityStore;
 		player.transform.position = currentCheckpoint.transform.position;
 		player.enabled = true;
 		player.GetComponent<Renderer> ().enabled = true;
+		Instantiate (respawnParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
 	}
 }
