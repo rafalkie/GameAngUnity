ecee5c9088f4bab8dada161ebd5ef3c352487972
lckvv <lckorone@gmail.com>
2018-06-05 12:13:32 +0200

    Update

--- GameAngUnity/New Unity Project/Assets/Scripts/wordPickUp.cs
+++ GameAngUnity/New Unity Project/Assets/Scripts/wordPickUp.cs
@@ -6,12 +6,16 @@ public class wordPickUp : MonoBehaviour {
 
 	public int pointsToAdd;
 
+	public AudioSource wordSoundEffect;
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.GetComponent<playerControler> () == null)
 			return;
 
 		scoreManager.AddPoints (pointsToAdd);
+
+		wordSoundEffect.Play ();
 
 		Debug.Log ("Zebrane");
 
